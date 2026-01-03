8c070fc4a9b81d64bcb5ba8e95fb679162cd4b79
Ben Clarke <ben@arrayofstars.com>
2026-01-03 17:21:01 -0500

    Pull calendar / bt stuff together in main

--- src/main.rs
+++ src/main.rs
@@ -1,42 +1,13 @@
-use std::time::Duration;
-
-use btleplug::api::{Central as _, Manager as _, Peripheral, ScanFilter};
-use btleplug::platform::Manager;
-use tokio::time;
-
-const TARGET_DEVICE: &str = "DoorSign";
+mod bt;
+mod calendar;
 
 #[tokio::main]
 async fn main() -> anyhow::Result<()> {
-    let uuid = uuid::Builder::from_u128(0);
-    let manager = Manager::new().await?;
-    let adapters = manager.adapters().await?;
-    let adapter = adapters.iter().nth(0).expect("No bluetooth adapter found");
+    let status = calendar::CalendarInfo::fetch_current_status().await?;
+    let ser = serde_json::to_string(&status)?;
 
-    let info = adapter.adapter_info().await?;
+    println!("{}", ser);
 
-    println!("Found bluetooth device: {}", info);
-
-    if let Err(e) = adapter.start_scan(ScanFilter::default()).await {
-        eprintln!("Couldn't start bt scan: {}", e);
-    }
-
-    time::sleep(Duration::from_secs(2)).await;
-
-    let peripherals = adapter.peripherals().await?;
-
-    for p in &peripherals {
-        let properties = p.properties().await?;
-        let is_target = properties.iter().any(|x| {
-            x.local_name
-                .as_ref()
-                .is_some_and(|k| k.contains(TARGET_DEVICE))
-        });
-
-        if is_target {}
-    }
-
-    adapter.stop_scan().await?;
-
+    bt::write_data("Whatever".as_bytes()).await?;
     Ok(())
 }
