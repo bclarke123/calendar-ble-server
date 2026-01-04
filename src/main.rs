664eb940dc1c6a193d40b8f183f825f644cf47e1
Ben Clarke <ben@arrayofstars.com>
2026-01-04 17:52:31 -0500

    Rename watch_for_device

--- src/main.rs
+++ src/main.rs
@@ -10,7 +10,7 @@ mod calendar;
 async fn main() -> anyhow::Result<()> {
     let (tx, rx) = watch::channel::<Option<CalendarInfo>>(None);
 
-    let bt_handle = tokio::spawn(bt::watch_for_device(rx));
+    let bt_handle = tokio::spawn(bt::run(rx));
     let cal_handle = tokio::spawn(calendar::sync_task(tx));
 
     let _ = join(bt_handle, cal_handle).await;
