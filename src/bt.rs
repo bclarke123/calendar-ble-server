21e8cd6de47f5919848a5402adf164f493f15fe5
Ben Clarke <ben@arrayofstars.com>
2026-01-04 18:45:00 -0500

    Fix up event loop, serialization, logical flow

--- src/bt.rs
+++ src/bt.rs
@@ -26,67 +26,54 @@ pub async fn run(rx: watch::Receiver<Option<CalendarInfo>>) -> ! {
 
     adapter.start_scan(filter.clone()).await.expect("Couldn't start bluetooth scan");
     let mut last_update = Instant::now() - Duration::from_secs(COOLDOWN_SEC);
+    let mut events = adapter.events().await.expect("Couldn't get bluetooth events");
 
-    loop {
-        let mut events = adapter.events().await.expect("Couldn't get bluetooth events");
+    println!("Bluetooth listener running");
 
-        while let Some(event) = events.next().await {
-            match event {
-                CentralEvent::DeviceDiscovered(id) | CentralEvent::DeviceUpdated(id) => {
-                    let peripheral = adapter.peripheral(&id).await.expect("Couldn't get peripheral");
-                    let props = match peripheral.properties().await.expect("Couldn't get peripheral properties") {
-                        Some(props) => props,
-                        None => continue
-                    };
+    while let Some(event) = events.next().await {
+        match event {
+            CentralEvent::DeviceDiscovered(id) | CentralEvent::DeviceUpdated(id) => {
+                let peripheral = adapter.peripheral(&id).await.expect("Couldn't get peripheral");
+                let props = match peripheral.properties().await.expect("Couldn't get peripheral properties") {
+                    Some(props) => props,
+                    None => continue
+                };
 
-                    let local_name = props.local_name.unwrap_or_default();
-                    if !local_name.contains(TARGET_DEVICE) {
-                        continue;
-                    }
-
-                    if Instant::now() - last_update < Duration::from_secs(COOLDOWN_SEC) {
-                        continue;
-                    }
-
-                    println!("Got wakeup call, sending latest data");
-
-                    adapter.stop_scan().await.ok();
-
-                    let data = match get_payload(&rx) {
-                        Ok(str) => str,
-                        Err(e) => {
-                            println!("Error serializing calendar data: {}", e);
-                            adapter.start_scan(filter.clone()).await.expect("Couldn't start bluetooth scan");
-                            continue
-                        }
-                    };
-
-                    if let Err(e) = write_data(&peripheral, data.as_bytes(), uuid).await {
-                        println!("Error writing calendar data: {}", e);
-                    } else {
-                        last_update = Instant::now();
-                    }
-
-                    adapter.start_scan(filter.clone()).await.expect("Couldn't start bluetooth scan");
+                let local_name = props.local_name.unwrap_or_default();
+                if !local_name.contains(TARGET_DEVICE) {
+                    continue;
                 }
-                _ => {}
+
+                if Instant::now() - last_update < Duration::from_secs(COOLDOWN_SEC) {
+                    continue;
+                }
+
+                println!("Got wakeup call, sending latest data");
+
+                let str_data = match &*rx.borrow() {
+                    Some(obj) => serde_json::to_string(obj).unwrap_or_default(),
+                    None => continue
+                };
+
+                adapter.stop_scan().await.ok();
+
+                if let Err(e) = write_data(&peripheral, str_data.as_bytes(), uuid).await {
+                    println!("Error writing calendar data: {}", e);
+                } else {
+                    println!("Wrote calendar data!");
+                    last_update = Instant::now();
+                }
+
+                adapter.start_scan(filter.clone()).await.expect("Couldn't start bluetooth scan");
             }
+            _ => {}
         }
     }
+
+    unreachable!();
 }
-
-fn get_payload(rx: &watch::Receiver<Option<CalendarInfo>>) -> anyhow::Result<String> {
-    let obj = rx.borrow().clone();
-    let str = serde_json::to_string(&obj)?;
-
-    println!("New calendar info, sending to device");
-    Ok(str)
-}
-
-// async fn watch_for_device()
 
 async fn write_data<P: Peripheral>(peripheral: &P, data: &[u8], uuid: Uuid) -> anyhow::Result<()> {
-
     time::timeout(Duration::from_secs(5), peripheral.connect()).await??;
     peripheral.discover_services().await?;
 
