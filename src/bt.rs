99d5c713ca24d1519d2cfb1d47a7aed55a7ee021
Ben Clarke <ben@arrayofstars.com>
2026-01-06 07:42:50 -0500

    Formatting, don't filter for uuids (yet)

--- src/bt.rs
+++ src/bt.rs
@@ -1,8 +1,8 @@
 use std::time::Duration;
 
-use btleplug::api::{CentralEvent, WriteType};
 use btleplug::api::{Central as _, Manager as _, Peripheral, ScanFilter};
-use btleplug::platform::{Manager};
+use btleplug::api::{CentralEvent, WriteType};
+use btleplug::platform::Manager;
 use futures::StreamExt;
 use tokio::sync::watch;
 use tokio::time::{self, Instant};
@@ -17,32 +17,53 @@ const COOLDOWN_SEC: u64 = 30;
 pub async fn run(rx: watch::Receiver<Option<CalendarInfo>>) -> ! {
     let uuid = uuid::Uuid::parse_str(TARGET_UUID_STR).expect("Invalid UUID");
 
-    let manager = Manager::new().await.expect("Couldn't create bluetooth manager");
-    let adapters = manager.adapters().await.expect("Couldn't get bluetooth adapters");
+    let manager = Manager::new()
+        .await
+        .expect("Couldn't create bluetooth manager");
+    let adapters = manager
+        .adapters()
+        .await
+        .expect("Couldn't get bluetooth adapters");
     let adapter = adapters.first().expect("No bluetooth adapter found");
 
-    let mut filter = ScanFilter:: default();
-    filter.services.push(uuid);
+    let mut filter = ScanFilter::default();
+    // filter.services.push(uuid);
 
-    adapter.start_scan(filter.clone()).await.expect("Couldn't start bluetooth scan");
+    adapter
+        .start_scan(filter.clone())
+        .await
+        .expect("Couldn't start bluetooth scan");
     let mut last_update = Instant::now() - Duration::from_secs(COOLDOWN_SEC);
-    let mut events = adapter.events().await.expect("Couldn't get bluetooth events");
+    let mut events = adapter
+        .events()
+        .await
+        .expect("Couldn't get bluetooth events");
 
     println!("Bluetooth listener running");
 
     while let Some(event) = events.next().await {
         match event {
             CentralEvent::DeviceDiscovered(id) | CentralEvent::DeviceUpdated(id) => {
-                let peripheral = adapter.peripheral(&id).await.expect("Couldn't get peripheral");
-                let props = match peripheral.properties().await.expect("Couldn't get peripheral properties") {
+                let peripheral = adapter
+                    .peripheral(&id)
+                    .await
+                    .expect("Couldn't get peripheral");
+                let props = match peripheral
+                    .properties()
+                    .await
+                    .expect("Couldn't get peripheral properties")
+                {
                     Some(props) => props,
-                    None => continue
+                    None => continue,
                 };
 
                 let local_name = props.local_name.unwrap_or_default();
+
                 if !local_name.contains(TARGET_DEVICE) {
                     continue;
                 }
+
+                println!("Event from {}", local_name);
 
                 if Instant::now() - last_update < Duration::from_secs(COOLDOWN_SEC) {
                     continue;
@@ -52,7 +73,7 @@ pub async fn run(rx: watch::Receiver<Option<CalendarInfo>>) -> ! {
 
                 let str_data = match &*rx.borrow() {
                     Some(obj) => serde_json::to_string(obj).unwrap_or_default(),
-                    None => continue
+                    None => continue,
                 };
 
                 adapter.stop_scan().await.ok();
@@ -64,7 +85,10 @@ pub async fn run(rx: watch::Receiver<Option<CalendarInfo>>) -> ! {
                     last_update = Instant::now();
                 }
 
-                adapter.start_scan(filter.clone()).await.expect("Couldn't start bluetooth scan");
+                adapter
+                    .start_scan(filter.clone())
+                    .await
+                    .expect("Couldn't start bluetooth scan");
             }
             _ => {}
         }
@@ -86,7 +110,9 @@ async fn write_data<P: Peripheral>(peripheral: &P, data: &[u8], uuid: Uuid) -> a
 
     println!("Writing status...");
 
-    peripheral.write(target_char, data, WriteType::WithResponse).await?;
+    peripheral
+        .write(target_char, data, WriteType::WithResponse)
+        .await?;
 
     peripheral.disconnect().await?;
 
